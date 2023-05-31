207d55868527ac78ba5eccec4778474b235fce9d
pavel-kovylnikov <pavel.kovylnikov@jetrockets.com>
2023-05-31 13:27:45 +0400

    test

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'get_process_mem', '~> 0.2.7'
+gem 'faker', '~> 3.2'
--- main.rb
+++ main.rb
@@ -1,14 +1,25 @@
 require 'benchmark'
+require 'get_process_mem'
 
 require_relative 'lib/csv_generator'
 require_relative 'lib/tasks/user_timing'
 require_relative 'lib/tasks/task_timing'
 
+
+
 file_path = 'tmp/data.csv'
 # CsvGenerator.new(file_path).call
-user_timing_time = Benchmark.measure { UserTiming.new(file_path).print }
-task_timing_time = Benchmark.measure { TaskTiming.new(file_path).print }
+first_check = GetProcessMem.new.bytes
+# user_timing_time = Benchmark.measure { UserTiming.new(file_path).print }
+UserTiming.new(file_path).print
+second_check = GetProcessMem.new.bytes
+# task_timing_time = Benchmark.measure { TaskTiming.new(file_path).print }
+TaskTiming.new(file_path).print
+third_check = GetProcessMem.new.bytes
 
-puts '               user     system      total        real'
-puts "UserTiming: #{user_timing_time}"
-puts "TaskTiming: #{task_timing_time}"
+# puts '               user     system      total        real'
+# puts "UserTiming: #{user_timing_time}"
+# puts "TaskTiming: #{task_timing_time}"
+
+puts "Memory usage after UserTiming: #{second_check - first_check} bytes"
+puts "Memory usage after TaskTiming: #{third_check - second_check} bytes"
